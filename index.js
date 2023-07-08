7e670e903e7c0a8ee92e7d670bdfda6fc5e28dcb
Ryan Zillini <nyrtrading134@gmail.com>
2023-07-08 14:24:27 -0400

    setup server and posts-router

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it here
+const server = require("./api/server");
+
+const PORT = 8000;
+
+server.listen(8000, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
