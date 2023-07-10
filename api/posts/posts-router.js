5cfb4842e7b43aa5f768ceda9fc66df7403f6d4c
Ryan Zillini <nyrtrading134@gmail.com>
2023-07-10 13:46:53 -0400

    completed get requests

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,6 +1,58 @@
 // implement your posts router here
 const express = require("express");
+const Post = require("./posts-model");
 
-const router = express.Router;
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    if (posts) {
+      res.json(posts);
+    }
+  } catch {
+    res
+      .status(500)
+      .json({ message: "The posts information could not be retrieved" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await Post.findById(id);
+    if (!post) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
+    } else {
+      res.json(post);
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "The post information could not be retrieved" });
+  }
+});
+
+// router.post("/", async (req, res) => {
+//   const post = req.body;
+//   if (!post.tile || !post.contents) {
+//     res
+//       .status(400)
+//       .json({ message: "Please provide title and contents for the post" });
+//   } else {
+//     try {
+//       const newPost = await Post.insert(post);
+//       res.status(201).json(newPost);
+//     } catch (err) {
+//       res
+//         .status(500)
+//         .json({
+//           message: "There was an error while saving the post to the database",
+//         });
+//     }
+//   }
+// });
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,17 +2,16 @@
 // require your posts router and connect it here
 
 const express = require("express");
-
-const server = express();
 const postsRouter = require("./posts/posts-router");
+const server = express();
 
 server.use(express.json());
 
 server.use("/api/posts", postsRouter);
 
 server.use("*", (req, res) => {
-  res.status(404).json({
-    message: "not found",
+  res.status(200).json({
+    message: "Hello from server",
   });
 });
 
