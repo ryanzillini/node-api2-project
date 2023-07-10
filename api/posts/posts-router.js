3e78c2bd52ed186774391ea74fa024cd67043aa4
Ryan Zillini <nyrtrading134@gmail.com>
2023-07-10 17:57:04 -0400

    completed delete requests

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -35,27 +35,6 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// router.post("/", (req, res) => {
-//   const { title, contents } = req.body;
-//   if (!title || !contents) {
-//     res.status(400).json({
-//       message: "Please provide title and contents for the post",
-//     });
-//   } else {
-//     Post.insert({ title, contents })
-//       .then(({ id }) => {
-//         return Post.findById(id);
-//       })
-//       .then((post) => {
-//         res.status(201).json(post);
-//       })
-//       .catch((err) => {
-//         res.status(500).json({
-//           message: "There was an error while saving the post to the database",
-//         });
-//       });
-//   }
-// });
 router.post("/", async (req, res) => {
   const { title, contents } = req.body;
   if (!title || !contents) {
@@ -76,6 +55,50 @@ router.post("/", async (req, res) => {
   }
 });
 
-// router.put('/:id', as)
+// router.put("/:id", async (req, res) => {
+//   const { id } = req.params;
+//   const { title, contents } = req.body;
+//   if (!title || !contents) {
+//     res
+//       .status(400)
+//       .json({ message: "Please provide title and contents to the post" });
+//   } else {
+//     try {
+//       if (!id) {
+//         res
+//           .status(404)
+//           .json({ message: "The post with the specified ID does not exist" });
+//       } else {
+//         const updatedPost = Post.update(id, { title, contents }).then(
+//           ({ id }) => {
+//             return Post.findById(id);
+//           }
+//         );
+//         res.status(200).json(updatedPost);
+//       }
+//     } catch (err) {
+//       res
+//         .status(500)
+//         .json({ message: "The post information could not be modified" });
+//     }
+//   }
+// });
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const currentPost = await Post.findById(id);
+  if (!currentPost) {
+    res
+      .status(404)
+      .json({ message: "The post with the specified ID does not exist" });
+  } else {
+    try {
+      await Post.remove(id);
+      res.json(currentPost);
+    } catch (err) {
+      res.status(500).json({ message: "The post could not be removed" });
+    }
+  }
+});
 
 module.exports = router;
